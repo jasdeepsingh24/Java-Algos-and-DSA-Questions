e59347e49ff2db3343ee718b592f067bdd35f12b
Jasdeep Singh <jasdeepsingh2405@gmail.com>
2023-01-05 19:18:52 +0530

    Custom Queue Implementation

--- src/DSA/CustomQueue2.java
+++ src/DSA/CustomQueue2.java
@@ -1,49 +1,47 @@
 package DSA;
 
-public class CustomQueue {
+public class CustomQueue2 {
     private int maxSize;
     private long[] qArray;
     private int front;
     private int rear;
-    private int nItems;
 
-    public CustomQueue(int maxSize) {
-        this.maxSize = maxSize;
-        this.qArray = new long[maxSize];
-        this.front = 0;
-        this.rear = -1;
-        this.nItems = 0;
+    public CustomQueue2(int maxSize) {
+        this.maxSize = maxSize + 1;
+        qArray = new long[maxSize];
+        front = 0;
+        rear = -1;
     }
 
     public void insert(long item) {
         if (rear == maxSize - 1)
             rear = -1;
         qArray[++rear] = item;
-        nItems++;
     }
 
     public long remove() {
         long temp = qArray[front++];
         if (front == maxSize)
             front = 0;
-        nItems--;
         return temp;
     }
 
-    public long peekFront()
-    {
-        return qArray[]
+    public long peek() {
+        return qArray[front];
     }
 
     public boolean isEmpty() {
-        return nItems == 0;
+        return (rear + 1 == front || (front + maxSize - 1 == rear));
     }
 
     public boolean isFull() {
-        return nItems == maxSize;
+        return (rear + 2 == front || (front + maxSize - 2 == rear));
     }
 
     public int size() {
-        return nItems;
+        if (rear >= front)
+            return rear - front + 1;
+        else
+            return (maxSize - front) + (rear - 1);
     }
 }
